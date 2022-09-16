7c54bd0322fee91bf3d745cdb8240df623b13087
NeilMcDeloitte <neilmclean@deloitte.co.uk>
2022-09-16 17:22:34 +0100

    Specify node version in package

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <>
       <h1>Homepage</h1>
-      <p>
+      <p className='govuk-body'>
         Test app for evaluating the development of forms with JSON Forms and
         React.
       </p>
